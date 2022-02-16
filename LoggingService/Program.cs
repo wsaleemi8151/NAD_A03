c6ce8fdce72a097b2575e6875140c4c848a8c030
Gursharan Singh <gursharansinghsandhu352@gmail.com>
2022-02-16 11:24:23 -0500

    Git clone successful

--- LoggingService/Program.cs
+++ LoggingService/Program.cs
@@ -10,6 +10,7 @@ namespace LoggingService
     static class Program
     {
         /// <summary>
+        /// 
         /// The main entry point for the application.
         /// </summary>
         static void Main()
