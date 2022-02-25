59b09383d8617be7419b65774671a0713bc8796d
Gursharan Singh <gursharansinghsandhu352@gmail.com>
2022-02-25 00:16:59 -0500

    final changes to client

--- PythonClient/LoggingClient.py
+++ PythonClient/LoggingClient.py
@@ -1,13 +1,14 @@
-
+# programmers - Gursharan Singh - Waqar Ali Saleemi
+# file - LoggingClient.py
 import socket
 from sys import argv
 from xml.etree.ElementTree import tostring
 
 target_host = input("Enter the host IP: ")
 target_port = input("Enter the target port: ")
+target_port = int(target_port)
 great = "ok good"
 msg = great
-print(argv[0])
 
 # create a socket connection
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,28 +16,38 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # let the client connect
 client.connect((target_host, target_port))
 
-
-
 Fcode = 0
-Fcode = input("Enter the Facility code[0-23]: ")
-Fcode = int(Fcode)
-Lcode = 0
-Lcode = input("Enter the severity Level[0-7]: ")
-Lcode = int(Lcode)
 
-Pcode = int(Fcode*8 + Lcode)
+# loop until user choose to
+while(Fcode != -1):
 
-Pcode = str(Pcode)
+    # get user input for Facility code
+    Fcode = 0
+    Fcode = input("Enter the Facility code[0-23]or [-1 to exit]: ")
+    Fcode = int(Fcode)
 
-Message = input("Enter the message to be sent: ")
+    if(Fcode == -1):
+        break
 
+    # get user input for severity level code
+    Lcode = 0
+    Lcode = input("Enter the severity Level[0-7]: ")
+    Lcode = int(Lcode)
 
-msg = Pcode + "|" + Message
+    Pcode = int(Fcode*8 + Lcode)
 
-# send some data
-client.sendall(msg.encode())
+    Pcode = str(Pcode)
 
-# get some data
-response = client.recv(4096)
+    Message = input("Enter the message to be sent: ")
 
-print(response.decode())
+    # message to be sent
+    msg = Pcode + "|" + Message
+
+    # send some data
+    client.sendall(msg.encode())
+
+    # get some data
+    response = client.recv(4096)
+
+    # server response
+    print(response.decode())
